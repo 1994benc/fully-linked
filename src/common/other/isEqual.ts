abcb11f70d8b7b3b7f751ac078d371c6425e4c8f
Ben Chomsang <43471788+1994benc@users.noreply.github.com>
2021-12-30 16:35:00 +0000

    Change how fast equal is imported

--- src/common/other/isEqual.ts
+++ src/common/other/isEqual.ts
@@ -1,4 +1,4 @@
-import * as equal from 'fast-deep-equal/es6'
+import * as equal from 'fast-deep-equal/es6/index'
 export function areObjectsEqual<T>(lhs: any, rhs: any): boolean {
     return equal(lhs, rhs)
 }

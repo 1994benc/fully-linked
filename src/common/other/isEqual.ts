d2f8d5ee0d4c8b8364fe1b6c36ae2c1351c28848
Ben Chomsang <43471788+1994benc@users.noreply.github.com>
2021-12-30 16:19:03 +0000

    Change deep equal library

--- src/common/other/isEqual.ts
+++ src/common/other/isEqual.ts
@@ -1,6 +1,4 @@
-
-var deepEqual = require('deep-equal')
-
+import * as equal from 'fast-deep-equal'
 export function areObjectsEqual<T>(lhs: any, rhs: any): boolean {
-    return deepEqual(lhs, rhs)
+    return equal(lhs, rhs)
 }
